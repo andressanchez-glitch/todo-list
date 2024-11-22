d583b0bac0fd7726a82cb53fdbadddc0c0c80342
andressanchez-glitch <thesanchezandres99@gmail.com>
2024-11-22 21:53:33 +0100

    Stop SetInterval
    non salva se non ci sono elementi visibili nella lista

--- js/app.js
+++ js/app.js
@@ -1,117 +1,161 @@
-console.log('ciao bello');
+/*  Esercizio 27a
 
-//si poteva scrivere anche così:
-//document.getElementById('add-button').addEventListener('click,', function(){});
+Modificare il progetto to-do-js in modo tale che:
 
-//Get the input field
+  1. salvi automaticamernte la lista dopo un certo periodo di tempo
+  2. si possa aggiungere un elemento alla lista premendo invio da tastiera o con il click del mouse  */
+
+console.log('collegato')
+
 var addButton = document.getElementById('add-button');
 
-//Execute a function when the user presses a key on the keyboard
 addButton.addEventListener('keypress', function (event) {
-    //if the user presses the 'Enter' key on the keyboard
-    if (event.key === "Enter") {
-        //cancel the default action, if needed
-        event.preventDefault();
-        addToDoItem();
-    }
-    else {
-        addButton.click().addToDoItem();
-    }
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addItem();
+  }
+  else {
+    addButton.addItem();
+  };
 
 });
 
-let clearButton = document.getElementById('clear-completed-button');
-clearButton.addEventListener('click', function () {
-    clearCompletedToDoItems();
+let deleteButton = document.getElementById('delete_db');
+deleteButton.addEventListener('click', function () {
+  clearButton();
 });
 
-let emptyButton = document.getElementById('empty-button')
-emptyButton.addEventListener('click', emptyList);
+let deleteDbAll = document.getElementById('delete_all');
+deleteDbAll.addEventListener('click', function () {
+  clearAll();
+});
 
-let saveButton = document.querySelector('#save-button')
-saveButton.addEventListener('click', saveList);
+let saveButton = document.querySelector('#save');
+saveButton.addEventListener('click', function () {
+  saveList();
+});
 
+var inputBox = document.getElementById('input-list');
 
-//variabile per il selettore HTML che ho come id todo-entry-box
-var toDoEntryBox = document.getElementById('todo-entry-box');
+var hiddenList = document.getElementById('list-ol');
 
-// variabile del selettore HTML che ha come id todo-list
-var toDoList = document.getElementById('todo-list');
+//invocazione
 
-function newToDoItem(itemText, completed) { // qui dichiaro la funzione 
-    let toDoItem = document.createElement('li');
-    let toDoText = document.createTextNode(itemText);
+function newAddItem(itemText, completed) {
+  let itemInvented = document.createElement('li');
+  let textplot = document.createTextNode(itemText);
 
-    toDoItem.appendChild(toDoText);/* rig 14-15 abbiamo definito la creaxione del listitem, quello che scrivo nel input va 
-   a todotext e quando faccio appendchild sto prendendo i listitem e li sto appiopado i valori di item che sono quelli scritti nel input */
+  itemInvented.appendChild(textplot);
 
+  if (completed) {
+    itemInvented.classList.add('completed')
+  }
+  hiddenList.appendChild(itemInvented);
+  itemInvented.addEventListener('dblclick', function () {
+    toggleItemState.call(itemInvented);
+  });
 
-    if (completed) {//se è completato 
-        toDoItem.classList.add('completed')
-    }
-    toDoList.appendChild(toDoItem);
-    toDoItem.addEventListener('dblclick', toggleToDoItemState);//dbclick doppio click
-}
-
-function addToDoItem() {
-    let itemText = toDoEntryBox.value;
-    newToDoItem(itemText, false); // invocazione della fuzione che setta il valore e lo mettiamo dentro a false, guarda se è compltede true oppure no false
-}
-
-function toggleToDoItemState() {
-    if (this.classList.contains('completed')) {//quando chiamo la funzione faccio un controllo per vedere se l'elemto 
-        this.classList.remove('completed');  //che chiama la funzione in questo caso toggle ha completed o no come classe tramite this 
-    }
-    else {
-        this.classList.add('completed');
-    }
 }
 
 
-function clearCompletedToDoItems() {
-    let completedItems = toDoList.getElementsByClassName('completed');
-
-    while (completedItems.length > 0) {
-        completedItems.item(0).remove();//andiamo a rimuovere il primo elemnto dalla lista
-    }
+function addItem() {
+  let itemText = inputBox.value;
+  newAddItem(itemText, false);
 }
 
-function emptyList() {
-    let toDoItem = toDoList.children
 
-    while (toDoItem.length > 0) {
-        toDoItem.item(0).remove();
-    }
+function toggleItemState() {
+  if (this.classList.contains('completed')) {
+    this.classList.remove('completed');
+  }
+  else {
+    this.classList.add('completed');
+  }
+}
+
+function clearButton() {
+  let clearItems = hiddenList.getElementsByClassName('completed');
+
+  for (let i = clearItems.length - 1; i >= 0; i--) {
+    clearItems.item(i).remove();
+  }
+}
+
+function clearAll() {
+  let dbItem = hiddenList.children
+
+  while (dbItem.length > 0) {
+    dbItem.item(0).remove();
+  }
 }
 
 function saveList() {
-    let toDos = [];
-    for (let i = 0; i < toDoList.children.length; i++) {
-        let toDo = toDoList.children.item(i);//item è un elemnto che ci aiuta a recuperare un milesimo del nostro giro
+  let toDos = [];
+  for (let i = 0; i < hiddenList.children.length; i++) {
+    let toDo = hiddenList.children.item(i);
 
-        var toDoInfo = {
-            "task": toDo.innerText, // riesco a fare questo perchè ho in mano gli elemnti di riga 90
-            "completed": toDo.classList.contains('completed')// che ho ciclato con il ciclo for e so il valore che ho scritto a schermo 
-        };
+    var toDoInfo = {
+      "task": toDo.innerText,
+      "completed": toDo.classList.contains('completed')
+    };
+    toDos.push(toDoInfo);
 
-        toDos.push(toDoInfo);
-        
-    }
-    console.log(toDos);
-    localStorage.setItem('toDos', JSON.stringify(toDos));
+  }
+  console.log(toDos);
+  localStorage.setItem('toDos', JSON.stringify(toDos));
 }
 
-//per caricare la lista per vederla nel broswers
 
-function loadList(){
-    if(localStorage.getItem('toDos') != null){// != null controllo per vedere che il valore non sia nullo
-        let toDos = JSON.parse(localStorage.getItem('toDos'));
+//per fare in modo che la lista non si salvi se è vuota
+function stopGoInterval() {
+  let intervallo = setInterval(saveList, 2000);
 
-        for(let i = 0; i < toDos.length; i++){ 
-            let toDo = toDos[i];
-            newToDoItem(toDo.task, toDo.completed)//task xk è la chiave il primo elemnto che abbiamo memorizzato
-        }
+  let stopInterval = document.getElementById('list-ol').children;
+  console.log(stopInterval);
+
+  if (stopInterval.length == 0) {//la condizione dice che siano vuoti se la lughezza della lista è 0 quindi vuota
+    clearInterval(intervallo);
+    console.log('clean')
+  }
+  else {
+    saveList();
+    console.log('save')
+  }
+}
+setInterval(stopGoInterval, 1000);
+//caricare la lista per vederla nel broswers
+
+function loadList() {
+  if (localStorage.getItem('toDos') != null) {
+    let toDos = JSON.parse(localStorage.getItem('toDos'));
+
+    for (let i = 0; i < toDos.length; i++) {
+      let toDo = toDos[i];
+      newAddItem(toDo.task, toDo.completed)
     }
+  }
 }
 
 loadList();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
